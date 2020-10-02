00d7850d304c7ded72bd9ae1b08f13fee8b2f1ea
Tom Pope <popetr90@gmail.com>
2020-10-02 10:12:42 +0100

    adding an Error Boundary to the application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import './App.css';
+// this is the modular approach to using CSS
+import styles from './App.module.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBounday'
 // import Radium, { StyleRoot } from 'radium'; // this allows you to use media and hover queries in your inline styles!
-import styled from 'styled-components';
+// import styled from 'styled-components';
 // Another great library you can use is styled-components.com
 // you need to import css into JS. React deals with this. 
 
 // cool special syntax below that uses props to change the styling dependant on the state. You can see we parsed the props into the <StyledButton> wrapper below. 
-const StyledButton = styled.button`
-  background-color: ${props => props.alt ? 'red' : 'green'};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
+// const StyledButton = styled.button`
+//   background-color: ${props => props.alt ? 'red' : 'green'};
+//   color: white;
+//   font: inherit;
+//   border: 1px solid blue;
+//   padding: 8px;
+//   cursor: pointer;
   
-  &:hover {
-    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-    color: black;
-  }
-`;
+//   &:hover {
+//     background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+//     color: black;
+//   }
+// `;
 
 class App extends Component {
   // State can only be used in class based components
@@ -44,6 +46,7 @@ class App extends Component {
     };
     // we use the spread operator here to avoid mutating data and so creating a copy
     person.name = event.target.value;
+    // event.target.value is the value the user entered
 
     const persons = [...this.state.persons];
     persons[personIndex] = person;
@@ -65,33 +68,25 @@ class App extends Component {
   }
   
   render () {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    };
+
 // the above is inline styling, like you would use in HTML. Careful as the properties need to be STRINGS!
     let persons = null;
+    let btnClass = [styles.button];
 
     if (this.state.showPersons) {
       persons = (
         <div>
           {/* instead of listing every person out, you can use map to loop through the array and print what we need */}
           {this.state.persons.map((person, index) => {
-            return <Person 
+            return <ErrorBoundary key={person.id}>
+              <Person 
               click={() => this.deletePersonHandler(index)}
               name={person.name} 
               age={person.age}
               // adding a key property element so react can understand which elements changed and which didnt
-              key={person.id} 
+               
               changed={(event) => this.nameChangedHandler(event, person.id)} />
+            </ErrorBoundary>
           })}
         </div>
       );
@@ -101,6 +96,9 @@ class App extends Component {
           //   backgroundColor: 'salmon',
           //   color: 'black'
           // };
+
+          // Below is the syling used with modular CSS
+          btnClass.push(styles.Red);
       }
     // this is a generic class list
     const classes = [];
@@ -114,15 +112,15 @@ class App extends Component {
 
     return (
       // Here we are wrapping our entire code in the StyleRoot radium component so you can access the media queries
-        <div className="App">
+        <div className={styles.App}>
           <h1>Hi Im a React App</h1>
           {/* Youll see the classes varibale stored above is used here. To display multiple classes, you need to add the join element */}
           <p className={classes.join(' ')}>This is really working!</p>
           {/* the below syntax this.switchNameHandler is not preferred over the bind syntax below.  */}
           {/* Below is using the styledbutton variable styles above and we are parsing in a state to change the style based on state. */}
-          <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>
+          <button className={btnClass.join(' ')} onClick={this.togglePersonsHandler}>
             Toggle Persons
-          </StyledButton>        
+          </button>        
           {persons}
         </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,33 +1,37 @@
 import React from 'react';
-import './Person.css';
-import styled from 'styled-components';
+import styles from './Person.module.css';
+// import styled from 'styled-components';
 // import Radium from 'radium';
 // the above import was added for purposes of linking the css
 
 // below you will see the work of styled components. It is saved to a variable and then you wrap all the content in the variable below. 
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 0 15px;
-    padding: 16px;
-    text-align: center;
+// const StyledDiv = styled.div`
+//     width: 60%;
+//     margin: 16px auto;
+//     border: 1px solid #eee;
+//     box-shadow: 0 0 15px;
+//     padding: 16px;
+//     text-align: center;
  
-    @media (min-width: 500px) {
-       width: 450px;
-    }
- `;
+//     @media (min-width: 500px) {
+//        width: 450px;
+//     }
+//  `;
 
 const person = ( props ) => {
+    const rnd = Math.random();
+        if (rnd > 0.7) {
+            throw new Error('Something went wrong');
+        }
     return (
         // <div className="Person" style={style}> When using radium
         // Below is the correct code for styled components
-            <StyledDiv>
+            <div className={styles.Person}>
                 <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
                 <p>{props.children}</p> 
                 <input type="text" onChange={props.changed} value={props.name}/>
                 {/* the value= updates the name to whatever it is changed to in the state.  */}
-            </StyledDiv>
+            </div>
     )
 };
 
